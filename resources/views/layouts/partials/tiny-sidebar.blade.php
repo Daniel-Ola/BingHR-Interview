ae52d1fd5c8efc019acba08a8d2cb903bac34580
Daniel Olanrewaju <danorelanre@gmail.com>
2022-04-25 21:45:44 +0100

    sidebar model

---
+++ resources/views/layouts/partials/tiny-sidebar.blade.php
@@ -0,0 +1,22 @@
+<div id="tiny-sidebar">
+
+    <div class="d-flex align-items-start flex-column bd-highlight" style="height: 100%;">
+
+        <div class="d-flex flex-column mb-auto show-box-2" style="margin-top: 5rem">
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+        </div>
+
+        <div class="d-flex flex-column">
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+            <i class="fa fa-search p-2"></i>
+        </div>
+    </div>
+
+
+</div>
