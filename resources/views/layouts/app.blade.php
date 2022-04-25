cd2e167250b235d6227a02d10e1cbfc97b65230d
Daniel Olanrewaju <danorelanre@gmail.com>
2022-04-25 23:27:01 +0100

    topbar model

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,13 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @stack("custom-styles")
 </head>
 <body>
     <div id="app" class="d-flex">
         @include("layouts.partials.sidebar")
 
-        <div>
+        <div style="background-color: #F9FBFD" class="w-100">
             @include("layouts.partials.topbar")
             @yield('content')
         </div>
--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -1,3 +1,58 @@
-<nav>
-    <h3>topnav</h3>
+<nav class="container mx-auto">
+
+    <div class="d-flex justify-content-between align-items-center mt-3" style="width: 100%;">
+        <div class="d-flex g-3 me-auto align-items-center">
+            <div class="col-auto" style="margin-right: 3rem">
+                <h3>Users</h3>
+            </div>
+            <div class="col-auto" style="margin-right: 1.5rem">
+
+                <div class="position-relative" style="width: 100px; height: 42px;">
+                    @include("layouts.components.NiceSelect.nice-select", [
+                                'placeholder' => 'Year',
+                                'options' => ['2022', '2021', '2020']
+                            ])
+                </div>
+
+            </div>
+            <div class="col-auto">
+
+                <div class="input-group" style="width: 300px; background-color: #fff; border-radius: 50px">
+                    <input type="text" class="form-control" id="nav-search" placeholder="Search...">
+                    <div class="position-absolute top-0 bottom-0" style="right: 10px; color: #DFE4EE">
+                        <div class="d-flex align-items-center h-100">
+                            <i class="fa fa-search"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="">
+            <div class="d-flex flex-row">
+                <div class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </div>
+                <div class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </nav>
